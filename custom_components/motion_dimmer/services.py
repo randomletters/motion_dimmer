e502c362f85c7e10c248a0387c720d18c94d4407
randomletters <randomletters@users.noreply.github.com>
2024-04-10 00:22:07 +0000

    Cancel periodic timer as well.

--- custom_components/motion_dimmer/services.py
+++ custom_components/motion_dimmer/services.py
@@ -83,6 +83,7 @@ async def async_service_finish_timer(hass: HomeAssistant, call: ServiceCall):
     for data in get_data(hass, call).values():
         switch: MotionDimmerSwitch = data.switch_object
         await switch.async_schedule_callback()
+        switch.cancel_periodic_timer()
         switch.cancel_timer()
 
 
